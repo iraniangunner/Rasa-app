7d813cb3879a57985abe8cf94161bb03ac832b68
Fateh Moonesi <53290111+iraniangunner@users.noreply.github.com>
2022-11-05 02:43:08 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  // darkMode: "class",
+  theme: {
+    extend: {
+      screens: {
+        sm: "576px",
+
+        md: "768px",
+
+        lg: "992px",
+
+        xl: "1200px",
+
+        "2xl": "1400px",
+      },
+      backgroundImage: {
+        "layout-pattern": "url('./images/pattern.png')",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+});
